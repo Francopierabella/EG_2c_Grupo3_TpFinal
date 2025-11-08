5d5e3723c30e124064aa755f21d40c1e5d4ce792
Franco <francopierabella10@gmail.com>
2025-11-07 23:43:45 -0300

    Vuelvo a la conexion anterior

--- src/consultaSql.php
+++ src/consultaSql.php
@@ -2,11 +2,10 @@
 
 function consultaSql($consulta): bool|mysqli_result {
     $conn = mysqli_connect(
-        getenv('DB_HOST'),
-        getenv('DB_USER'),
-        getenv('DB_PASS'),
-        getenv('DB_NAME'),
-        getenv('DB_PORT')
+        "localhost",
+        "root",
+        "",
+        "onixcenter"
     );
 
     if (!$conn) {
